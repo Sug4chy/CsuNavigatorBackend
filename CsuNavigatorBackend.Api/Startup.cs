3e16b096cab9b5b7d33fc686c006fcac663d8a9f
Степан <sb.9226981433@gmail.com>
2024-02-24 08:12:59 +0500

    init commit

---
+++ CsuNavigatorBackend.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace CsuNavigatorBackend.Api;
+
+public class Startup(IWebHostEnvironment env)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthorization();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+    }
+}
